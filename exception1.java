ddd97313f949a59a0e31c1850c1120bea856176e
Lucas-LC93 <75119661+Lucas-LC93@users.noreply.github.com>
2025-05-21 20:07:16 +0200

    Update exception1.java

--- exception1.java
+++ exception1.java
@@ -2,7 +2,7 @@ package com.excepciones;
 
 public class exception1 {
     public static void main(String[] args) {
-        int a = 10;
+        int a = 20;//prueba de como funciona Github
         int b = 0;
         int resultado = a / b; // Esto generará una excepción de tipo ArithmeticException
         System.out.println("Resultado: " + resultado);
